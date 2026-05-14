0fe29b4389e638b34da9880843881824401d27fa
Babatunde Adegboye <babatundeadegboye2008@gmail.com>
2026-05-14 11:21:12 +0100

    feat: Changed auth middleware from bearer token to cookies

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ import  { verifyAccessToken } from '../utils/jwt.js';
 
 
 const authMiddleware = async (req, res, next) => {
-    const authHeader = req.headers['authorization'];
+   /*  const authHeader = req.headers['authorization'];
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({
             success: false,
@@ -27,8 +27,28 @@ const authMiddleware = async (req, res, next) => {
             success: false,
             message: "Unauthorized"
         }); 
+    } */
+    let authHeader = req.cookies?.accessToken;
+    if(!authHeader){
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized"
+        })
     }
-}
+    try {
+        const token = authHeader;
+        const decoded = await verifyAccessToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error("Authentication error:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+ }
+
 
 
 export { authMiddleware as authenticate };
